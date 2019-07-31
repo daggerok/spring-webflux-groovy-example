e1de14d6c4e3c5e9b7f7ea34d87debd8ebeaa7e2
Maksim Kostromin <daggerok@gmail.com>
2019-07-31 04:09:51 +0300

    Update SpringWebfluxGroovyApplicationTests.groovy

--- src/test/groovy/com/github/daggerok/messages/SpringWebfluxGroovyApplicationTests.groovy
+++ src/test/groovy/com/github/daggerok/messages/SpringWebfluxGroovyApplicationTests.groovy
@@ -1,11 +1,34 @@
 package com.github.daggerok.messages
 
-import org.junit.jupiter.api.Test
+import com.github.daggerok.messages.rest.Handlers
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
 
 @SpringBootTest
 class SpringWebfluxGroovyApplicationTests {
 
+	@Autowired(required = false)
+	Handlers handlers
+
+	def "when context is loaded then all expected beans are created"() {
+		expect: "Handlers bean is created"
+			handlers
+	}
+
+	def "ololo trololo"() {
+		given:
+			int a = 1
+		and:
+			int b = 2
+		when:
+			int c = a + b
+		then:
+			c == 3
+	}
+	
 	@Test
-	void "ololo trololo"() {}
+	void "troololo ololo"() {
+		println 'this should be executed only with junit runnet...'
+	}
 }
