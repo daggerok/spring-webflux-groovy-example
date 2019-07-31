9761f86b65fc5d882d3cb265bda2e4b9ce15553f
Maksim Kostromin <daggerok@gmail.com>
2019-07-31 04:20:54 +0300

    Update SpringWebfluxGroovyApplicationTests.groovy

--- src/test/groovy/com/github/daggerok/messages/SpringWebfluxGroovyApplicationTests.groovy
+++ src/test/groovy/com/github/daggerok/messages/SpringWebfluxGroovyApplicationTests.groovy
@@ -1,5 +1,6 @@
 package com.github.daggerok.messages
 
+import org.junit.jupiter.api.Test
 import com.github.daggerok.messages.rest.Handlers
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
